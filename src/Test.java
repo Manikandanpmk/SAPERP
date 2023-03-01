d167612474fb0e4a69c672e9f408a4def4237d35
Manikandan Palanisamy <manikandanp@cdata.com>
2023-03-01 14:41:26 +0530

    Fucnction ReadTableFunction="RFC_READ_TABLE2"

--- src/Test.java
+++ src/Test.java
@@ -5,9 +5,9 @@ import java.sql.Statement;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-        Connection conn = DriverManager.getConnection("jdbc:saperp:ConnectionScheme=ApplicationServer;Host=/H/10.0.1.61/S/3300;User=DDIC;Password=c`460653078;Client=001");
+        Connection conn = DriverManager.getConnection("jdbc:saperp:ConnectionScheme=ApplicationServer;Host=/H/10.0.1.61/S/3300;User=DDIC;Password=c`460653078;Client=001;ReadTableFunction=RFC_READ_TABLE2");
         Statement stat = conn.createStatement();
-        boolean ret = stat.execute("SELECT MANDT, MATNR FROM MARA");
+        boolean ret = stat.execute("SELECT * FROM BUT000_BCS");
         if (ret) {
             ResultSet rs = stat.getResultSet();
             while (rs.next()) {
