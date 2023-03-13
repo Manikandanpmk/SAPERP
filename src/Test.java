e440596d4c7e790a50db665366f52fcd79f120c4
Manikandan Palanisamy <manikandanp@cdata.com>
2023-03-13 15:13:14 +0530

    Fucnction ReadTableFunction="RFC_READ_TABLE2"

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
 public class Test {
     public static void main(String[] args) throws Exception {
         Connection conn = DriverManager.getConnection("jdbc:saperp:ConnectionScheme=ApplicationServer;Host=/H/10.0.1.61/S/3300;User=DDIC;Password=c`460653078;Client=001;ReadTableFunction=RFC_READ_TABLE2");
